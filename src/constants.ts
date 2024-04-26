91b7861a93c55a3a99d852815c618f973a3fab2c
nathan <liuyunathan@gmail.com>
2024-04-26 23:55:42 +0800

    bitlayer

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from '@ethersproject/bignumber'
 
-export const PERMIT2_ADDRESS = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
+export const PERMIT2_ADDRESS = '0x3EA312569078c754cf907623A434589b3ac15D2A'
 
 export const MaxUint48 = BigNumber.from('0xffffffffffff')
 export const MaxUint160 = BigNumber.from('0xffffffffffffffffffffffffffffffffffffffff')
